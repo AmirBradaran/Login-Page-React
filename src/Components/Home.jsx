4cdebdda79cf2b046dbaa3542c3ec25298c4f7e6
Amir <baradaran13085@gmail.com>
2024-12-31 01:54:06 +0330

    Set Home Part
    And finally set a simple Home page and done .

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import "./Home.css"
 export default function Home() {
   return (
-    <div>
-      
+    <div className='home-Page'>
+      <section className='home'>
+      <h2>If You Liked My Project</h2>
+      <h4>See My Other Projects :) </h4>
+      </section>
     </div>
   )
 }
