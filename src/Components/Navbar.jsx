7d376b9184d01706cc7da321428ef1c006665a2d
Amir <baradaran13085@gmail.com>
2024-12-31 01:41:04 +0330

    Create Navbar
    Create a simple Navbar for login project and set a simple style for Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React, { useContext } from "react";
+import AuthContext from "../Utils/AuthContext";
+import "./NavBar.css"
 export default function Navbar() {
+  const { token, handleToken } = useContext(AuthContext);
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className="navBar">
+      <div className="navBar-Section">
+        <h1 className="navBar-Title">Login React Page</h1>
+        <section className="navBar-Li">
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Apps</a>
+        </li>
+        {!token ? (
+          <li>
+            <a> Login </a>
+          </li>
+        ) : (
+          <li>
+            <button onClick={()=>handleToken(null)} className="navBar-Li-Btn"> Logout </button>
+          </li>
+        )}
+        </section>
+      </div>
+    </nav>
+  );
 }
